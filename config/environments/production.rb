889f583af4d1b651f2df01736ee42561386176ae
Paul Simpson <prsimp@gmail.com>
2012-04-21 15:07:29 -0500

    Fixing more sendgrid typos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Nycorbust::Application.configure do
   # Action Mailer Settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:        "stmp.sendgrid.net",
-    port:           587,
+    address:        "smtp.sendgrid.net",
+    port:           "587",
     authentication: :plain,
     user_name:      ENV["SENDGRID_USERNAME"],
     password:       ENV["SENDGRID_PASSWORD"],
