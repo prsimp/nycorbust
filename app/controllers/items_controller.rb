b12ed517f7db440999d812ca0f6bb543983fcbad
Paul Simpson <prsimp@gmail.com>
2012-04-23 07:47:30 -0500

    Completed admin namespace

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,49 +1,11 @@
 class ItemsController < ApplicationController
-
-  skip_before_filter :authorized_user, only: [:index, :show]
+  skip_before_filter :authorized_user
 
   def index
-    @items = Item.order("name asc").all
+    @items = Item.available.by_name
   end
 
   def show
     @item = Item.find(params[:id])
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-    @item.build_item_images
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update_attributes(params[:item])
-      flash[:success] = "#{@item.name} successfully updated."
-      redirect_to @item
-    else
-      render 'edit'
-    end
-  end
-
-  def new
-    @item = Item.new
-    @item.build_item_images
-  end
-
-  def create
-    @item = Item.new(params[:item])
-    if @item.save
-      flash[:success] = @item.name + " successfully added."
-      redirect_to @item
-    else
-      @item.rebuild_item_images
-      render 'new'
-    end
-  end
-
-  def destroy
-    item = Item.find(params[:id]).destroy
-    flash[:success] = "#{item.name} deleted."
-    redirect_to 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Nycorbust::Application.routes.draw do
 
-  resources :categories
-  resources :items
+  resources :categories, only: [:index, :show]
+  resources :items, only: [:index, :show]
+
+  namespace :admin do
+    resources :categories, except: [:show, :destroy]
+    resources :items, except: :show
+  end
+
   resources :users
   resources :sessions, only: [ :new, :create, :destroy ]
   resources :contact, only: [:new, :create ]
+
 
   match '/login'  => 'sessions#new'
   match '/logout' => 'sessions#destroy'
