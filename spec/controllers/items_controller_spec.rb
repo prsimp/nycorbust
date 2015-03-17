041db1c341dcdd9a02a0aa9d41677e07da1920fe
Paul Simpson <prsimp@gmail.com>
2015-03-16 23:08:12 -0400

    More what.

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -10,8 +10,14 @@ describe ItemsController do
   end
 
   describe "GET 'show'" do
+    let(:category) { Category.create!(name: 'Foo') }
+    let(:item) do
+      category.items.create!(name: 'Bar', description: 'Baz', price: 10)
+    end
+
     it "returns http success" do
-      get 'show'
+      get 'show', id: item.id
+
       response.should be_success
     end
   end
